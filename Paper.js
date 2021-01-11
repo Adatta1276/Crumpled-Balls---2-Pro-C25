7a97bb50ae8ebbf09a8cdcbd962be569433d2e50
Adatta1276 <73747859+Adatta1276@users.noreply.github.com>
2021-01-11 15:24:03 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -27,7 +27,7 @@ class Paper {
 			translate(paperpos.x, paperpos.y);
 			rectMode(CENTER)
 			//strokeWeight(4);
-			fill(255,0,255)
+			//fill(255,0,255)
 			imageMode(CENTER);
 			image(this.image, 0,0,this.width, this.height)
 			//ellipse(0,0,this.r, this.r);
